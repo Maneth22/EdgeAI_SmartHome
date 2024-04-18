c9d4fc356cb6631fb27acf274689540c0b797dcb
SachinHansaka <sachin.h@voxitec.com>
2024-04-18 21:45:22 +0530

    Update the model function & cleaned the code

--- Model_For_Object_Detection.py
+++ Model_For_Object_Detection.py
@@ -1,65 +1,54 @@
 from ultralytics import YOLO
 import cv2
 
-import random
 
-# Define the number of random numbers you want to generate
-num_numbers = 100
+def yolo_pre():
 
-# Use a for loop to generate and print random numbers
-for _ in range(num_numbers):
-    random_number = random.randint(1, 100)  # Generates a random number between 1 and 100
-    #print(random_number)
+    model = YOLO('yolov8n.pt')
+
+    # load video
+    cap = cv2.VideoCapture(0)
+
+    ret = True
+    # read frames
+
+    while ret:
+        ret, frame = cap.read()
+
+        if ret:
+            #results =  model.track(frame)
+            results = model.track(frame, persist=True)
+            
+            person_count = 0
+            
+            # check if object with id 1 is detected
+            for result in results:
+                
+                for c in result.boxes.cls:
+                    print(model.names[int(c)])
+                    
+                    if model.names[int(c)] == 'person':
+                        person_count += 1
+                        
+                    if person_count >= 1:
+                        cap.release()
+                        cv2.destroyAllWindows()
+                    
+            print(f"Number of persons detected: {person_count}")
+                
+            # plot results
+            frame_ = results[0].plot()
+
+            # visualize
+            cv2.imshow('frame', frame_)
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+
+    cap.release()
+    cv2.destroyAllWindows()
     
-    if random_number == 30:
-        print(random_number)
-        # load yolov8 model
-        model = YOLO('yolov8n.pt')
+    return person_count
 
-        # load video
-        cap = cv2.VideoCapture(0)
 
-        # Create an empty list to store the names of detected objects
-        detected_objects = []
-
-        ret = True
-        # read frames
-
-        while ret:
-            ret, frame = cap.read()
-
-            if ret:
-                # Perform inference
-                #results = model(frame)
-                # detect objects
-                results =  model.track(frame)
-                #results = model.track(frame, persist=True)
-                
-                person_count = 0
-                
-                # check if object with id 1 is detected
-                for result in results:
-                    
-                    for c in result.boxes.cls:
-                        print(model.names[int(c)])
-                        
-                        if model.names[int(c)] == 'person':
-                            person_count += 1
-                            print("Hello There is a person!!!")
-                        #print(type(model.names[int(c)]))
-                        
-                print(f"Number of persons detected: {person_count}")
-                    
-                # plot results
-                frame_ = results[0].plot()
-
-                # visualize
-                cv2.imshow('frame', frame_)
-                if cv2.waitKey(25) & 0xFF == ord('q'):
-                    break
-
-        # Print the list of detected objects
-        print(f"Detected objects: {detected_objects}")
-
-        cap.release()
-        cv2.destroyAllWindows()
+num_count = yolo_pre()
+print(num_count)
